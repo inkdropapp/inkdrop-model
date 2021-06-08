38d203d1379d6380843e10451400702b3f9a2ee3
Takuya Matsuyama <hi@craftz.dog>
2021-06-08 15:06:42 +0900

    test(file): too large image

--- test/file.js
+++ test/file.js
@@ -34,3 +34,40 @@ test('basic validation', t => {
   const valid2 = validateFile(data)
   t.is(valid2, true)
 })
+
+test('too large image', t => {
+  const data: File = {
+    createdAt: 1503124076660,
+    name: 'Dog.jpg',
+    contentType: 'image/jpeg',
+    contentLength: 1503124076660,
+    publicIn: ['note:Bkl_9Vubx'],
+    _attachments: {
+      index: {
+        digest: 'md5-j+1LGuZoOdF1G5EFuv8+xA==',
+        content_type: 'image/jpeg',
+        data: 'data',
+        length: 1503124076660
+      }
+    },
+    _id: 'file:By8_nQtce',
+    _rev: '14-813af5085bb6a2648c3f0aca37fc821f'
+  }
+  const valid = validate(data)
+  t.is(valid, false)
+  t.deepEqual(validate.errors, [
+    {
+      instancePath: '/contentLength',
+      keyword: 'maximum',
+      message: 'must be <= 10485760',
+      params: {
+        comparison: '<=',
+        limit: 10485760
+      },
+      schemaPath: '#/properties/contentLength/maximum'
+    }
+  ])
+  const valid2 = validateFile(data)
+  t.is(valid2, false)
+  t.log(validateFile.errors)
+})
