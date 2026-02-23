16a1f813ef4a624c7d6c96efeba7ba1980cc13e8
Takuya Matsuyama <hi@craftz.dog>
2026-02-23 12:04:16 +0900

    chore(dev): bump up

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   transform: {
     '^.+\\.(t|j)sx?$': 'ts-jest'
   },
+  transformIgnorePatterns: ['/node_modules/(?!nanoid)'],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testEnvironment: 'node'
